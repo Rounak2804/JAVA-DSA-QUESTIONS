a793776463391e4c2342b3162b5155aca8cbced5
Rounak Raj <rounak00003@gmail.com>
2025-12-21 23:10:53 +0530

    Time: 6 ms (19.39%) | Memory: 45.5 MB (15.96%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
@@ -1,34 +1,16 @@
-import java.util.HashMap;
-
+import java.util.Arrays;
 class Solution {
-    public int findGCD(int[] nums) {
-
-        // Step 1: Put array elements into HashMap
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int n : nums) {
-            map.put(n, map.getOrDefault(n, 0) + 1);
+    public int findGCD(int[] arr) {
+        Arrays.sort(arr);
+        int n = arr.length;
+        int small = arr[0];
+        int largest = arr[n-1];
+        int gcd = 1;   
+        for(int i = 1;i<=largest;i++){
+        if(small%i==0 && largest%i==0){
+            gcd = i;
         }
-
-        // Step 2: Find min and max keys
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-
-        for (int key : map.keySet()) {
-            min = Math.min(min, key);
-            max = Math.max(max, key);
         }
-
-        // Step 3: Return GCD of min and max
-        return gcd(min, max);
-    }
-
-    // Euclidean Algorithm
-    private int gcd(int a, int b) {
-        while (b != 0) {
-            int temp = a % b;
-            a = b;
-            b = temp;
-        }
-        return a;
+        return gcd;
     }
 }
