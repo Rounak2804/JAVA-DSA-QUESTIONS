841b6194e3d85c36dbe97d6ad73b4970f8d02d2f
Rounak Raj <rounak00003@gmail.com>
2026-08-08 11:54:46 +0530

    Time: 2 ms (94.66%) | Memory: 47.4 MB (77.97%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,21 +1,18 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        // Arrays.sort(nums);
+        
         int res[] = new int[2];
         boolean arr[] = new boolean[nums.length];
-
-        for (int x : nums) {
-            if (arr[x - 1]) {
-                res[0] = x; 
-            } else {
-                arr[x - 1] = true;
+        for(int i = 0;i<nums.length;i++){
+            int index = nums[i];
+            if(arr[index - 1] == true){
+                res[0] =  nums[i];
             }
+            arr[index - 1] = true;
         }
-
-        for (int i = 0; i < arr.length; i++) {
-            if (!arr[i]) {
+        for(int i = 0;i<arr.length;i++){
+            if(!arr[i]){
                 res[1] = i + 1;
-                
             }
         }
         return res;
